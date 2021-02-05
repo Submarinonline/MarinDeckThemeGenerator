9f5189888e07597402b85a856b6f87903cca6e42
hakunagi <hakunagi.rei@gmail.com>
2021-02-05 22:16:08 +0900

    css出力機能を追加

--- web/js/mdtg.js
+++ web/js/mdtg.js
@@ -1,5 +1,4 @@
-let themeCss = {};
-
+let tdCss = {};
 $(function () {
   $('input[name="td-theme"]:radio').change(function () {
     let val = $(this).val();
@@ -10,19 +9,29 @@ $(function () {
     }
   });
 
-  $('input.td-com-css').change(function () {
+  $('input.td-css').change(function () {
     let tdSel = $(this).data('sel');
     let tdProp = $(this).data('prop');
     let val = $(this).val();
     $('.td-preview').contents().find(tdSel).css(tdProp, val);
-    $(`input[data-sel=${tdSel}][data-prop=${tdProp}]`).val(val);
-  });
-
-  $('input.td-col-css').change(function () {
-    let tdSel = $(this).data('sel');
-    let tdProp = $(this).data('prop');
-    let val = $(this).val();
-    $('.td-preview').contents().find(tdSel).css(tdProp, val);
-    $(`input[data-sel=${tdSel}][data-prop=${tdProp}]`).val(val);
+    $(`input[data-sel="${tdSel}"][data-prop="${tdProp}"]`).val(val);
+    if(tdSel in tdCss === false) tdCss[tdSel] = [];
+    Object.keys(tdCss).forEach((key)=>{
+      tdCss[key].forEach((e)=>{
+        if(e.match(new RegExp(tdProp))) {
+          tdCss[key].splice(key, 1);
+        }
+      })
+    })
+    tdCss[tdSel].push(`${tdProp}:${val}!important;`);
+    let tdCssText = "";
+    Object.keys(tdCss).forEach((key)=>{
+      tdCssText = tdCssText + `${key}{`
+      tdCss[key].forEach((e)=>{
+        tdCssText = tdCssText + e;
+      })
+      tdCssText = tdCssText+"}\n"
+    })
+    $('.td-css-text').val(tdCssText);
   });
 });
