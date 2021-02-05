b46f1631504a8990aa064908081b7a00732dde52
hakunagi <hakunagi.rei@gmail.com>
2021-02-05 22:22:26 +0900

    他のプロパティも一緒に削除される不具合を修正

--- web/js/mdtg.js
+++ web/js/mdtg.js
@@ -15,22 +15,20 @@ $(function () {
     let val = $(this).val();
     $('.td-preview').contents().find(tdSel).css(tdProp, val);
     $(`input[data-sel="${tdSel}"][data-prop="${tdProp}"]`).val(val);
-    if(tdSel in tdCss === false) tdCss[tdSel] = [];
-    Object.keys(tdCss).forEach((key)=>{
-      tdCss[key].forEach((e)=>{
-        if(e.match(new RegExp(tdProp))) {
-          tdCss[key].splice(key, 1);
-        }
-      })
+    if (tdSel in tdCss === false) tdCss[tdSel] = [];
+    tdCss[tdSel].forEach((e) => {
+      if (e.match(new RegExp(tdProp))) {
+        tdCss[tdSel].splice(tdSel, 1);
+      }
     })
     tdCss[tdSel].push(`${tdProp}:${val}!important;`);
     let tdCssText = "";
-    Object.keys(tdCss).forEach((key)=>{
+    Object.keys(tdCss).forEach((key) => {
       tdCssText = tdCssText + `${key}{`
-      tdCss[key].forEach((e)=>{
+      tdCss[key].forEach((e) => {
         tdCssText = tdCssText + e;
       })
-      tdCssText = tdCssText+"}\n"
+      tdCssText = tdCssText + "}\n"
     })
     $('.td-css-text').val(tdCssText);
   });
